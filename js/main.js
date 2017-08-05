b000fd19c81c6e8f4333c7c9b8cd571a73d3e15b
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-04 22:34:00 -0400

    Añadimos id en HTML

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-var nombre = document.getElementById("").value;
-var apellido = document.getElementById("").value;
-var correo = document.getElementById("").value;
-var direccion = document.getElementById("").value;
-var telefono = document.getElementById("").value;
-var pack = document.getElementById("").value;
+var nombre = document.getElementById("nombre").value;
+var apellido = document.getElementById("apellido").value;
+var correo = document.getElementById("email").value;
+var direccion = document.getElementById("direccion").value;
+var telefono = document.getElementById("telefono").value;
+var pack = document.getElementById("pack").value;
+var check = document.getElementById("check").value;
+function mostrarData(){
+
+}
