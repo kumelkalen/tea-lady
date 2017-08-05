8ae4a1521e92b94c5b25cf53dc6fac9442f8c7c4
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-04 22:52:23 -0400

    Si se cumplen todas las condiciones pondremos los datos en el div vacío

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ var telefono = document.getElementById("telefono").value;
 var pack = document.getElementById("pack").value;
 var check = document.getElementById("check").value;
 function mostrarData(){
-	var cont = 0;
+	//Validaciones
+	var cont = 0;//contador
 	if(nombre != ""){
 		cont++;
 	}
@@ -28,4 +29,15 @@ function mostrarData(){
 	if(check.checked = true){
 		cont++; 
 	}
+	if(cont == 7){
+	//Llenamos el div con los datos
+		var registros = document.getElementById("clientes");//div vacío
+		var nomText = document.createTextNode("Nombre: "+nombre);
+		var apText = document.createTextNode("Apellido: "+apellido);
+		var emText = document.createTextNode("Email: "+email);
+		var diText = document.createTextNode("Direccion: "+direccion);
+		var teText = document.createTextNode("Teléfono: "+telefono);
+		var paText = document.createTextNode("Pack seleccionado: "+pack);
+	}
+	
 }
