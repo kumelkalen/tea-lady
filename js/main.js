bee39321c37366decd713ad4e1a573e958d9c538
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-05 00:09:58 -0400

    Añadimos estilos

--- js/main.js
+++ js/main.js
@@ -3,32 +3,40 @@ var apellido = document.getElementById("apellido").value;
 var correo = document.getElementById("email").value;
 var direccion = document.getElementById("direccion").value;
 var telefono = document.getElementById("telefono").value;
-var pack = document.getElementById("pack").value;
+var pack = document.getElementsByTagName("option");
 var check = document.getElementById("check").value;
 function mostrarData(){
 	//Validaciones
 	var cont = 0;//contador
 	if(nombre != ""){
 		cont++;
+		console.log(1);
 	}
 	if(apellido != ""){
 		cont++;
+		console.log(2);
 	}
 	if(correo != ""){
 		cont++;
+		console.log(3);
 	}
 	if(direccion != ""){
 		cont++;
+		console.log(4);
 	}
 	if(telefono != ""){
 		cont++;
+		console.log(5);
 	}
-	if(pack != 0){
+	if(pack.value != pack[0].value){
 		cont++;
+		console.log(6);
 	}
 	if(check.checked = true){
 		cont++; 
+		console.log(7);
 	}
+	console.log(cont);
 	if(cont == 7){
 	//Llenamos el div con los datos
 		var registros = document.getElementById("clientes");//div vacío
@@ -38,6 +46,14 @@ function mostrarData(){
 		var diText = document.createTextNode("Direccion: "+direccion);
 		var teText = document.createTextNode("Teléfono: "+telefono);
 		var paText = document.createTextNode("Pack seleccionado: "+pack);
+		registros.appendChild(nomText);
+		registros.appendChild(apText);
+		registros.appendChild(emText);
+		registros.appendChild(diText);
+		registros.appendChild(teText);
+		registros.appendChild(paText);
+	}else{
+		alert("No hemos podido registrar tus datos con éxito, vuelve a intentarlo por favor :0 .")
 	}
 	
 }
