5d14bbbbe5b16e0bc2dd75237d1f05f080059a7d
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-05 00:58:16 -0400

    Uso de innerHTML

--- js/main.js
+++ js/main.js
@@ -4,56 +4,44 @@ var correo = document.getElementById("email").value;
 var direccion = document.getElementById("direccion").value;
 var telefono = document.getElementById("telefono").value;
 var pack = document.getElementsByTagName("option");
-var check = document.getElementById("check").value;
+var check = document.getElementById("check");
 function mostrarData(){
 	//Validaciones
 	var cont = 0;//contador
 	if(nombre != ""){
 		cont++;
-		console.log(1);
+		//console.log(1);
 	}
 	if(apellido != ""){
 		cont++;
-		console.log(2);
+		//console.log(2);
 	}
 	if(correo != ""){
 		cont++;
-		console.log(3);
+		//console.log(3);
 	}
 	if(direccion != ""){
 		cont++;
-		console.log(4);
+		//console.log(4);
 	}
 	if(telefono != ""){
 		cont++;
-		console.log(5);
+		//console.log(5);
 	}
 	if(pack.value != pack[0].value){
 		cont++;
-		console.log(6);
+		//console.log(6);
 	}
-	if(check.checked = true){
+	if(check.checked == true){
 		cont++; 
-		console.log(7);
+		//console.log(7);
 	}
-	console.log(cont);
+	//console.log(cont);
 	if(cont == 7){
 	//Llenamos el div con los datos
 		var registros = document.getElementById("clientes");//div vacío
-		var nomText = document.createTextNode("Nombre: "+nombre);
-		var apText = document.createTextNode("Apellido: "+apellido);
-		var emText = document.createTextNode("Email: "+email);
-		var diText = document.createTextNode("Direccion: "+direccion);
-		var teText = document.createTextNode("Teléfono: "+telefono);
-		var paText = document.createTextNode("Pack seleccionado: "+pack);
-		registros.appendChild(nomText);
-		registros.appendChild(apText);
-		registros.appendChild(emText);
-		registros.appendChild(diText);
-		registros.appendChild(teText);
-		registros.appendChild(paText);
+		registros.innerHTML="Nombre: "+nombre+"<br>Apellido: "+apellido+"<br>Direccion: "+direccion+"<br>Teléfono: "+telefono+"<br>Pack seleccionado: "+pack;
 	}else{
 		alert("No hemos podido registrar tus datos con éxito, vuelve a intentarlo por favor :0 .")
 	}
-	
 }
