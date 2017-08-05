611e70b0f7dc86536108c83f47d2d78c39c81035
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-04 22:43:14 -0400

    Creamos validaciones que cuando esten correctas aumente un contador

--- js/main.js
+++ js/main.js
@@ -6,5 +6,26 @@ var telefono = document.getElementById("telefono").value;
 var pack = document.getElementById("pack").value;
 var check = document.getElementById("check").value;
 function mostrarData(){
-
+	var cont = 0;
+	if(nombre != ""){
+		cont++;
+	}
+	if(apellido != ""){
+		cont++;
+	}
+	if(correo != ""){
+		cont++;
+	}
+	if(direccion != ""){
+		cont++;
+	}
+	if(telefono != ""){
+		cont++;
+	}
+	if(pack != 0){
+		cont++;
+	}
+	if(check.checked = true){
+		cont++; 
+	}
 }
