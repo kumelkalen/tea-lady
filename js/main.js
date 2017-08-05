970c18dea51ab933e21084fca9efcb4c4fa3e5a9
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-04 22:21:44 -0400

    Obtenemos los valores de los input

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var nombre = document.getElementById("").value;
+var apellido = document.getElementById("").value;
+var correo = document.getElementById("").value;
+var direccion = document.getElementById("").value;
+var telefono = document.getElementById("").value;
+var pack = document.getElementById("").value;
